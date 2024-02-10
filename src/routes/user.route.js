c91e2cb1a92bda99d695f8456f91c7e4e0d0aef2
Gaurav Patil <gauravpatil97886@gmail.com>
2024-02-10 23:13:46 +0530

    add router

---
+++ src/routes/user.route.js
@@ -0,0 +1,8 @@
+// Importing the Router class from the Express library
+import { Router } from "express";
+
+// Creating an instance of the Router class
+const router = Router();
+
+// Exporting the router instance as the default export
+export default router;
