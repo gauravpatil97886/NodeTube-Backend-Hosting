b829135724ab0e1ddb0dac53fb2f5ff520e839e9
Gaurav Patil <gauravpatil97886@gmail.com>
2024-02-13 22:51:01 +0530

    add subscrption model

--- src/models/Subscription.model.js
+++ src/models/Subscription.model.js
@@ -15,3 +15,22 @@ const SubscriptionSchema = new Schema({
 const Subscription = mongoose.model("Subscription", SubscriptionSchema);
 
 export default Subscription;
+
+import mongoose ,{Schema} from "mongoose";
+
+
+const SubscriptionSchema = new Schema({ 
+  subscriber: {
+    type: Schema.Types.ObjectId, // The user who is subscribing
+    ref: "User"
+  },
+  channel: {
+    type: Schema.Types.ObjectId, // The user to whom the subscriber is subscribing
+    ref: "User"
+  }
+}, { timestamps: true });
+
+const Subscription = mongoose.model("Subscription", SubscriptionSchema);
+
+export default Subscription;
+
